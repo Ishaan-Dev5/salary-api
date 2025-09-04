2f51fb7a1e48e97887017c29b763462162c60921
Ishaan-Dev5 <ishaanaggarwal32@gmail.com>
2025-09-04 13:30:54 +0530

    Update SalaryApplicationTests.java

--- src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -5,6 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SalaryApplicationTests {
+	 @MockBean
+    private DataSource dataSource;   // Mock DB connection
+
+    @MockBean
+    private RedisTemplate redisTemplate;  // Mock Redis conne
 
 	@Test
 	void contextLoads() {
