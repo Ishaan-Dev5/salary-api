76ff347aa637f49d718b648a05f547fefeb64c98
Ishaan-Dev5 <ishaanaggarwal32@gmail.com>
2025-09-04 13:42:32 +0530

    Update SalaryApplicationTests.java

--- src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
+++ src/test/java/com/opstree/microservice/salary/SalaryApplicationTests.java
@@ -2,27 +2,25 @@ package com.opstree.microservice.salary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import javax.sql.DataSource;                     // For DataSource
-import org.springframework.boot.test.mock.mockito.MockBean;  // For @MockBean
+import javax.sql.DataSource;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
-// For RedisTemplate
-
 
 @SpringBootTest
 @ActiveProfiles("test")
 class SalaryApplicationTests {
-	 @MockBean
+
+    @MockBean
     private DataSource dataSource;   // Mock DB connection
 
     @MockBean
-    private RedisTemplate redisTemplate;  // Mock Redis conne
+    private RedisTemplate<String, Object> redisTemplate;  // Mock Redis connection
 
-	 @MockBean
-    private CassandraTemplate cassandraTemplate;
+    // Remove CassandraTemplate if you don't have dependency
+    // @MockBean
+    // private CassandraTemplate cassandraTemplate;
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
